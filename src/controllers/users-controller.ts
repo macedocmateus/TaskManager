94df6d4c39e917420af7ab2855c271d9f4fcb523
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-03-27 17:26:42 -0300

    add create an user

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,38 @@
+import { hash } from 'bcrypt'
 import { Request, Response } from 'express'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma.js'
+import { AppError } from '@/utils/AppError.js'
 
 class UsersController {
-  create(request: Request, response: Response) {
-    return response.json({ message: 'oii' })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.email(),
+      password: z.string().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } })
+
+    if (userWithSameEmail) {
+      throw new AppError('User with same email already exists')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+
+    const { password: _, ...userWithoutPassword } = user
+
+    return response.json(userWithoutPassword)
   }
 }
 
