c12b7971f562c9a293849a254081221a8f1ee905
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-03-28 15:47:24 -0300

    add create sessons

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,34 @@
+import { compare } from 'bcrypt'
+import { Request, Response } from 'express'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma.js'
+import { AppError } from '@/utils/AppError.js'
+
+class SessionsController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.email(),
+      password: z.string().min(6),
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    })
+
+    if (!user) {
+      throw new AppError('Invalid email or password', 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError('Invalid email or password', 401)
+    }
+
+    return response.json({ message: 'ok' })
+  }
+}
+
+export { SessionsController }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 
 import { usersRoutes } from '@/routes/users-routes.js'
+import { sessionsRoutes } from './sessions-routes.js'
 
 const routes = Router()
 routes.use('/users', usersRoutes)
+routes.use('/sessions', sessionsRoutes)
 
 export { routes }
