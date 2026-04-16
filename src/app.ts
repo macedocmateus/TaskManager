9ab39ad99b2f00dae3848e53c730d08cf741a73b
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-04-16 13:39:29 -0300

    init docs with swagger openapi

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
 import 'dotenv/config'
 
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
 import cors from 'cors'
 import express from 'express'
+import { load } from 'js-yaml'
+import { JsonObject, serve, setup } from 'swagger-ui-express'
 import { errorHandling } from '@/middlewares/error-handling.js'
 import { routes } from '@/routes/index.js'
+
+const swaggerDocument = load(
+  readFileSync(resolve('src/docs/swagger.yml'), 'utf-8'),
+) as JsonObject
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+
+app.use('/docs', serve, setup(swaggerDocument))
 
 app.use(errorHandling)
 
